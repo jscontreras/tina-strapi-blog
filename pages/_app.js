263680af26eb285b7b2a7822f9bc3440c9c212d4
Brent Mitton <bmitton@gatech.edu>
2020-06-30 09:44:03 -0400

    add providers to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '../styles/index.css'
+import "../styles/index.css";
+
+import {
+  StrapiMediaStore,
+  StrapiProvider,
+  TinaStrapiClient,
+} from "react-tinacms-strapi";
+import { TinaCMS, TinaProvider } from "tinacms";
+
+import { useMemo } from "react";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const cms = useMemo(
+    () =>
+      new TinaCMS({
+        sidebar: { hidden: true },
+        toolbar: { hidden: false },
+        apis: {
+          strapi: new TinaStrapiClient(),
+        },
+        media: {
+          store: new StrapiMediaStore(),
+        },
+      })
+  );
+  return (
+    <TinaProvider cms={cms}>
+      <StrapiProvider
+        onLogin={() => {
+          /* we'll come back to this */
+        }}
+        onLogout={() => {
+          /* we'll come back to this */
+        }}
+      >
+        <Component {...pageProps} />
+      </StrapiProvider>
+    </TinaProvider>
+  );
 }
